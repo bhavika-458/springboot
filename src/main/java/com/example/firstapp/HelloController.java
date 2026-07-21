83ac4b24316af9aab1de0e315d573e4b635047e5
bhavika-458 <bhavika030402@gmail.com>
2026-07-21 11:53:52 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/example/firstapp/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.firstapp;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HelloController{
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Spring Boot";
+    }
+    
+}
